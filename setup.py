e22a4a953cd1cd0efc3913e7786be3da81e2475e
Luke Pajer <31792929+The-Geology-Guy@users.noreply.github.com>
2020-10-27 14:16:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,36 @@
+from setuptools import setup
+import os
+import sys
 
+_here = os.path.abspath(os.path.dirname(__file__))
+
+if sys.version_info[0] < 3:
+    with open(os.path.join(_here, 'README.md')) as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+version = {}
+with open(os.path.join(_here, 'SLAM', 'version.py')) as f:
+    exec(f.read(), version)
+
+setup(
+    name='SLAM',
+    version=version['__version__'],
+    description=('Seismo-Lineament Analysis Method'),
+    long_description=long_description,
+    author='Luke Pajer',
+    author_email='bruce.wayne@example.com',
+    url='https://github.com/The-Geology-Guy/SLAM',
+    license='MPL-2.0',
+    packages=['SLAM'],
+   install_requires=[
+       'dependency==1.2.3',
+   ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.7'],
+    )
