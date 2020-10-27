2a1949cb84d58faf00d73d5359ed71bc80c544b8
Luke Pajer <31792929+The-Geology-Guy@users.noreply.github.com>
2020-10-27 13:57:39 -0400

    Update __init__.py

--- package/__init__.py
+++ package/__init__.py
@@ -1,1 +1,16 @@
+from .version import __version__
+from .SLAM import DEM
+from .SLAM import get_data
+from .SLAM import qFaults
+from .SLAM import swaths
+from .SLAM import SLAM_viz
 
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+    'DEM',
+    'get_data',
+    'qFaults',
+    'swaths',
+    'SLAM_viz',
+]
