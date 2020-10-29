ad95fde0622a765496dc25abd77d1b45cc3a2af9
Luke Pajer <31792929+The-Geology-Guy@users.noreply.github.com>
2020-10-29 13:58:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import sys
 
 _here = os.path.abspath(os.path.dirname(__file__))
 
-if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'description.rst')) as f:
-        long_description = f.read()
-else:
-    with open(os.path.join(_here, 'description.rst'), encoding='utf-8') as f:
-        long_description = f.read()
+#if sys.version_info[0] < 3:
+#    with open(os.path.join(_here, 'description.rst')) as f:
+#        long_description = f.read()
+#else:
+#    with open(os.path.join(_here, 'description.rst'), encoding='utf-8') as f:
+#        long_description = f.read()
 
 version = {}
 with open(os.path.join(_here, 'GEOSLAM', 'version.py')) as f:
@@ -19,7 +19,7 @@ setup(
     name='GEOSLAM',
     version=version['__version__'],
     description=('Seismo-Lineament Analysis Method'),
-    long_description=long_description,
+    long_description=('The seismo-lineament analysis method is a tool to spatially correlate a shallow-focus earthquake to the surface trace of the fault that generated it. SLAM is the intellectual property and work product of Vince Cronin. The GEOSLAM Python code was a simple translation from Vince Cronin\'s Mathematica files, where this translation was performed by Luke Pajer.'),
     author='Luke Pajer',
     author_email='luke.pajer@gmail.com',
     url='https://github.com/The-Geology-Guy/GEOSLAM',
